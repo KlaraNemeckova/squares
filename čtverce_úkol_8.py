3bfd5922b7abaeec1101fb07c0e8e43b26d7dfaa
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-07-07 20:31:23 +0200

    čtverec b

--- čtverce_úkol_8.py
+++ čtverce_úkol_8.py
@@ -1,5 +1,7 @@
+
 symbol = input("Symbol tvořící čtverec: ") + " "
 
+# a 
 def square_a(size):
     n = 0
     k = size
@@ -10,4 +12,17 @@ def square_a(size):
       
 square_a(int(input("Strana čtverce (sudé číslo): ")))
 
+# b
+
+symbol = input("Symbol tvořící čtverec: ") + " "
+
+def square_b(size):
+    n = 0
+    k = size
+    for i in range(size):
+       print(symbol * n + " " * k)
+       n += 1
+       k -= 1
+       
+square_b(int(input("Strana čtverce (sudé číslo): ")))
 
